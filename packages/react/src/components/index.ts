1455f6c370a009f7e352c5ca1ce9136fd17c72a7
Joshua Onwuzu <joshuaonwuzu75@gmail.com>
2025-07-14 14:38:49 +0100

    updated formulajs

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,6 +1,10 @@
 import Workbook from "./Workbook";
 
+export {
+  ERROR_MESSAGES_FLAG,
+  SERVICES_API_KEY,
+  // @ts-ignore
+} from "@fileverse-dev/formulajs/crypto-constants";
 export { Workbook };
 export type { WorkbookInstance } from "./Workbook";
-// @ts-ignore
-export * from "@fileverse-dev/formulajs/crypto-constants";
+export type { Cell, Sheet } from "@fileverse-dev/fortune-core";
